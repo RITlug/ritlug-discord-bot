8f1f5d00cbccf08ad3e46a3af116876f98e0f92b
tylermurphy534 <tylermurphy534@gmail.com>
2022-10-05 22:37:53 -0400

    update database mod

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,6 +1,12 @@
 mod roles;
 pub use roles::get_page;
+pub use roles::set_page;
+pub use roles::delete_page;
+pub use roles::get_page_amount;
 
-pub fn init() {
-  roles::init();
+use rusqlite::{Result};
+
+pub fn init() -> Result<()> {
+  roles::init()?;
+  Ok(())
 }
