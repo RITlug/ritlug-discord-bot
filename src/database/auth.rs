2cc18e780f4d59aae7566da01b2436d183634b77
Tyler Murphy <tylermurphy534@gmail.com>
2022-10-08 15:41:43 -0400

    add auth_data and guild_data to database

---
+++ src/database/auth.rs
@@ -0,0 +1,59 @@
+use rusqlite::{Connection, Result};
+
+fn get_connection() -> Result<rusqlite::Connection, rusqlite::Error> {
+  return Connection::open("database.db");
+}
+
+pub fn init() -> Result<()> {
+  let sql = "
+    CREATE TABLE IF NOT EXISTS auth_data (
+      guild_id BIGINT NOT NULL,
+      user_id BIGINT NOT NULL,
+      email VARCHAR(320) NOT NULL,
+      auth_date BITING NOT NULL,
+      PRIMARY KEY (guild_id,user_id)
+    );
+  ";
+  let conn = get_connection()?;
+  conn.execute(sql, ())?;
+  Ok(())
+}
+
+pub struct Row {
+  guild_id: u64,
+  user_id: u64,
+  email: String,
+  auth_date: u64
+}
+
+pub fn get_user(guild_id: &u64, user_id: &u64) -> Result<Option<Row>> {
+  let sql = "SELECT * FROM auth_data WHERE guild_id=? AND user_id=?";
+  let conn = get_connection()?;
+  let mut stmt = conn.prepare(sql)?;
+  let iter = stmt.query_map([guild_id, user_id], |row| {
+    Ok(Row {
+      guild_id: row.get(0)?,
+      user_id: row.get(1)?,
+      email: row.get(2)?,
+      auth_date: row.get(3)?
+    })
+  })?;
+  for row in iter {
+    return Ok(Some(row?))
+  }
+  Ok(None)
+}
+
+pub fn set_user(guild_id: &u64, user_id: &u64, email: &str, auth_date: &u64) -> Result<()> {
+  let sql = "REPLACE INTO auth_data (guild_id, user_id, email, auth_date) VALUES(?,?,?,?)";
+  let conn = get_connection()?;
+  conn.execute(sql, (&guild_id, &user_id, &email, &auth_date))?;
+  Ok(())
+}
+
+pub fn delete_user(guild_id: &u64, user_id: &u64) -> Result<()> {
+  let sql = "DELETE FROM auth_data WHERE guild_id = ? AND user_id = ?;";
+  let conn = get_connection()?;
+  conn.execute(sql, (&guild_id, &user_id))?;
+  Ok(())
+}
--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,7 +1,11 @@
 pub mod roles;
+pub mod auth;
+pub mod guild;
 
 use rusqlite::Result;
 pub fn init() -> Result<()> {
   roles::init()?;
+  auth::init()?;
+  guild::init()?;
   Ok(())
 }
