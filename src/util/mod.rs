022504e9abd53b51a63c456483423a739fbec8c3
TriMill <trimill012@gmail.com>
2023-01-24 20:14:56 -0500

    Consistent four space indentation

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,21 +3,21 @@ use poise::serenity_prelude as serenity;
 use crate::{Context, Error};
 
 pub async fn error(ctx: &Context<'_>, msg: &str) -> Result<(), Error> {
-  ctx.send(|b| b.ephemeral(true).embed(|b| b.description(msg).color(serenity::Color::RED))).await?;
-  Ok(())
+    ctx.send(|b| b.ephemeral(true).embed(|b| b.description(msg).color(serenity::Color::RED))).await?;
+    Ok(())
 }
 
 pub async fn critical_error(ctx: &Context<'_>, msg: &str) -> Result<(), Error> {
-  ctx.send(|b| b.ephemeral(true).embed(|b| b.title(":x: Fatal Error").description(msg).color(serenity::Color::RED))).await?;
-  Ok(())
+    ctx.send(|b| b.ephemeral(true).embed(|b| b.title(":x: Fatal Error").description(msg).color(serenity::Color::RED))).await?;
+    Ok(())
 }
 
 pub async fn get_guild_id(ctx: &Context<'_>) -> Result<u64, String> {
-  match ctx.guild_id() {
-    Some(x) => Ok(x.0),
-    None => {
-      error(&ctx, ":x: Failed to fetch guild id").await.unwrap();
-      return Err("Failed to fetch guild id".to_string())
+    match ctx.guild_id() {
+        Some(x) => Ok(x.0),
+        None => {
+            error(&ctx, ":x: Failed to fetch guild id").await.unwrap();
+            return Err("Failed to fetch guild id".to_string())
+        }
     }
-  }
 }
